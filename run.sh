c6f0fd08197cf5f45fea0acdd5896fa1053ab41f
Anthony Green <green@moxielogic.com>
2024-12-05 15:00:10 -0500

    refactor(run.sh): change prep.sh script file path

--- run.sh
+++ run.sh
@@ -94,9 +94,9 @@ if [ $? -eq 0 ]; then
     echo "(asdf:initialize-source-registry '(:source-registry :ignore-inherited-configuration (:tree #p\"$(cd ${SRCDIR}; pwd)/\")))" >> ~/.sbclrc ;
     echo ==== .sbclrc ===================================================================
     cat ~/.sbclrc
-    if test -f ../prep.sh; then
+    if test -f ./prep.sh; then
        echo ==== Running prep.sh script ====================================================;
-       ../prep.sh;
+       ./prep.sh;
     fi
     # Build each system
     for S in ${SYSTEMS}; do
